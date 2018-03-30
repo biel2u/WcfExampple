986333afbaab16f325168f9916fe3678cab466a5
Patryk Bielecki <p.patrykbielecki@gmail.com>
2018-03-30 19:33:37 +0200

    wcf auth example

---
+++ Wcf.Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Security;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wcf.Service;
+using System.Web.UI;
+using Wcf.Client.MyService;
+
+
+namespace Wcf.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new MyServiceClient())
+            {
+                client.ClientCredentials.UserName.UserName = "username";
+                client.ClientCredentials.UserName.Password = "password";
+                Console.WriteLine(client.GetMessage());
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Wcf.Service/Validation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Selectors;
+using System.Linq;
+using System.ServiceModel.Security;
+using System.Web;
+
+namespace Wcf.Service
+{
+    public class Validation : UserNamePasswordValidator
+    {
+        public override void Validate(string userName, string password)
+        {
+            if (userName == "username" && password == "password")
+                return;
+
+            throw new System.IdentityModel.Tokens.SecurityTokenException("Unknown Username or Password");
+        }
+    }
+}
